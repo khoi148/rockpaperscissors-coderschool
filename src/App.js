ead1c17ccb36d673b9c91844fb310f2415b9887b
khoi148 <khoiminhphan@yahoo.com>
2020-03-17 23:00:47 +0700

    transformed all the component functions to class syntax

--- src/App.js
+++ src/App.js
@@ -1,130 +1,158 @@
-import React, { useState } from "react";
+import React from "react";
 import ChoiceCards from "./components/ChoiceCards.js";
 import ChoiceButtons from "./components/ChoiceButtons.js";
 import "./App.css";
 import { choices, getRoundOutcome } from "./utils"; //choices is an array of obj. getROut is a function returning an array of 2 elements
-export default function App() {
-  /*Intro to useState() :
-    The initial value/argument will be assigned only on the initial render (if it’s a function, 
-      it will be executed only on the initial render). In subsequent renders (due to a 
-      change of state in the component or a parent component), the argument of the 
-      useState Hook will be ignored and the current value will be retrieved. */
-  const [state, setGameState] = useState(null); //use state returns [argument, itsOwnSetterMethod()] in an array
-  const [userChoice, setUserChoice] = useState(null);
-  const [computerChoice, setComputerChoice] = useState(null);
-  const [previousWinner, setPreviousWinner] = useState(null);
-  const [gameHistory, setGameHistory] = useState([]);
-  const [flawlessVictory, setFlawlessVictory] = useState([]);
-  const [userName, setUsername] = useState(null);
-  const [gameOn, setGameOn] = useState(false);
+// import { render } from "@testing-library/react";
 
-  console.log(previousWinner);
-  let promptColor =
-    previousWinner === "You"
-      ? "winner"
-      : previousWinner === "Computer"
-      ? "loser"
-      : previousWinner === "Tie"
-      ? "tie"
-      : "start";
+export default class App extends React.Component {
+  /*Intro to state :
+    The initial value/argument will be assigned only on the initial render (if it’s a function,
+      it will be executed only on the initial render). In subsequent renders (due to a
+      change of state in the component or a parent component), the argument of the
+      state in js classes, or useState() in js functions, will be ignored and the current value will be retrieved. 
+      Also know that when you set state, you do not immediately change your local copy of state. Wait until next
+      rerender to get updated value, or pass in a callback function to setState(..., callbackfunction())
+      which will let you interact with the updated state*/
+  constructor(props) {
+    super(props);
+    this.state = {
+      state: null,
+      userChoice: null,
+      computerChoice: null,
+      previousWinner: null,
+      gameHistory: [],
+      flawlessVictory: [],
+      userName: null,
+      gameOn: false
+    };
+    //use this if using hooks, and not JS classeses
+    // const [state, setGameState] = useState(null); //use state returns [argument, itsOwnSetterMethod()] in an array
+    // const [userChoice, setUserChoice] = useState(null);
+    // const [computerChoice, setComputerChoice] = useState(null);
+    // const [previousWinner, setPreviousWinner] = useState(null);
+    // const [gameHistory, setGameHistory] = useState([]);
+    // const [flawlessVictory, setFlawlessVictory] = useState([]);
+    // const [userName, setUsername] = useState(null);
+    // const [gameOn, setGameOn] = useState(false);
+  }
 
-  const onPlayerChoose = playerChoice => {
+  displayStartButton = () => {
+    if (this.state.gameOn === true) return "displayNone";
+    else return "";
+  };
+
+  promptColor = () => {
+    if (this.state.previousWinner === "You") return "winner";
+    else if (this.state.previousWinner === "Computer") return "loser";
+    else if (this.state.previousWinner === "Tie") return "tie";
+    else return "start";
+  };
+
+  onPlayerChoose = playerChoice => {
     let [result, compChoice] = getRoundOutcome(playerChoice); //returns an array with 2 elements. 1st element is 'victory|defeat|tie', 2nd element is 'rock|paper|scissor'
     if (result === "Victory!") {
-      setPreviousWinner("You");
+      this.setState({ previousWinner: "You" });
     } else if (result === "Defeat!") {
-      setPreviousWinner("Computer");
+      this.setState({ previousWinner: "Computer" });
     } else {
-      setPreviousWinner("Tie");
+      this.setState({ previousWinner: "Tie" });
     }
-
     const newUserChoice = choices[playerChoice]; //object like Rock: {name: , url: }
     const newComputerChoice = choices[compChoice];
-    setUserChoice(newUserChoice);
-    setComputerChoice(newComputerChoice);
-    setGameState(result);
-    gameHistory.push({ result: result, name: userName });
-    setGameHistory(gameHistory);
-
-    //length of the flawlessVictory array. Push returns length of array
-    let arrLength =
-      result === "Victory!"
-        ? flawlessVictory.push("win")
-        : flawlessVictory.push("nah");
-    if (arrLength === 3) {
-      if (flawlessVictory.every(item => item === "win")) {
-        setGameState("Flawless Victory");
+    this.setState({ userChoice: newUserChoice });
+    this.setState({ computerChoice: newComputerChoice });
+    this.setState({ state: result });
+    // cannot push directly to an array state, still need to update
+    this.setState(
+      {
+        gameHistory: this.state.gameHistory.concat({
+          result: result,
+          name: this.state.userName
+        })
+      },
+      () => {
+        let arrLength = this.state.gameHistory.length;
+        arrLength === 1 &&
+        this.state.gameHistory.every(item => item.result === "Victory!")
+          ? this.setState({ state: "Flawless Victory" })
+          : (arrLength = null); //assignment to make use of ternary operator
       }
-    }
-    setFlawlessVictory(flawlessVictory);
-    // console.log(promptColor);
-    // console.log('classNames: ', document.getElementById('test').className);
+    );
   };
 
-  const changeUsername = () => {
+  changeUsername = () => {
     let inputValue = document.getElementById("nameInput").value;
-    setUsername(inputValue);
+    this.setState({ userName: inputValue });
   };
 
-  const startGame = () => {
-    setGameOn(true);
+  startGame = () => {
+    this.setState({ gameOn: true });
   };
-  let displayStartButton = gameOn === true ? "displayNone" : "";
 
-  return (
-    <div className="App">
-      <div className="container">
-        <div className="row mb-3">
-          <div className="col-md-8 themed-grid-col">
-            <ChoiceCards
-              title="Computer"
-              winner={previousWinner}
-              imgURL={computerChoice && computerChoice.imgURL}
-            />
-            <h1 id="test" className={`${promptColor} border-0`}>
-              {state}
-            </h1>
-            <button
-              className={`btn btn-success rounded ${displayStartButton}`}
-              onClick={() => startGame()}
-            >
-              Start Game!
-            </button>
-            <ChoiceButtons onPlayerChoose={onPlayerChoose} gameOn={gameOn} />
-
-            <ChoiceCards
-              title="You"
-              winner={previousWinner}
-              imgURL={userChoice && userChoice.imgURL}
-            />
-          </div>
-
-          <div className="col-md-4 themed-grid-col p-3">
-            <div className="d-flex flex-row align-items-center bg-success p-3">
-              <label>Input name: {userName}</label>
-              <span className="p-3 ">
-                <input id="nameInput" className="border-0" />
-              </span>
-              <button onClick={() => changeUsername()}>Submit</button>
+  render() {
+    return (
+      <div className="App">
+        <div className="container">
+          <div className="row mb-3">
+            <div className="col-md-8 themed-grid-col">
+              <ChoiceCards
+                title="Computer"
+                winner={this.state.previousWinner}
+                imgURL={
+                  this.state.computerChoice && this.state.computerChoice.imgURL
+                }
+              />
+              <h1 id="test" className={`${this.promptColor()} border-0`}>
+                {this.state.state}
+              </h1>
+              {this.state.gameOn === false && ( //using conditional to choose to render an obj or not
+                <button
+                  className={`btn btn-success rounded ${this.displayStartButton()}`}
+                  onClick={() => this.startGame()}
+                >
+                  Start Game!
+                </button>
+              )}
+              {this.state.gameOn === true && (
+                <ChoiceButtons
+                  onPlayerChoose={this.onPlayerChoose}
+                  test={true}
+                />
+              )}
+              <ChoiceCards
+                title="You"
+                winner={this.state.previousWinner}
+                imgURL={this.state.userChoice && this.state.userChoice.imgURL}
+              />
             </div>
-            <div className="bg-warning">
-              <h3 className="m-0 py-2">History</h3>
-              <ul className="list-unstyled bg-warning">
-                {gameHistory.map(item => {
-                  console.log(item.name);
-                  let nameText =
-                    item.name === null ? "" : `Name: ${item.name},`;
-                  return (
-                    <li className="bg-light">
-                      {nameText} result: {item.result}
-                    </li>
-                  );
-                })}
-              </ul>
+
+            <div className="col-md-4 themed-grid-col p-3">
+              <div className="d-flex flex-row align-items-center bg-success p-3">
+                <label>Input name: {this.state.userName}</label>
+                <span className="p-3 ">
+                  <input id="nameInput" className="border-0" />
+                </span>
+                <button onClick={() => this.changeUsername()}>Submit</button>
+              </div>
+              <div className="bg-warning">
+                <h3 className="m-0 py-2">History</h3>
+                <ul className="list-unstyled bg-warning">
+                  {this.state.gameHistory.map(item => {
+                    let nameText =
+                      item.name === null ? "" : `Name: ${item.name},`;
+                    return (
+                      <li className="bg-light">
+                        {nameText} result: {item.result}
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
--- src/components/ChoiceButtons.js
+++ src/components/ChoiceButtons.js
@@ -1,29 +1,37 @@
 import React from "react";
 
-export default function ChoiceButtons(props) {
-  let displayChoiceButtons = props.gameOn === true ? "" : "displayNone";
-  return (
-    <div className="container d-flex justify-content-center">
-      <div className={displayChoiceButtons}>
-        <button
-          className="btn btn-success btn-lg"
-          onClick={() => props.onPlayerChoose("rock")}
-        >
-          Rock
-        </button>
-        <button
-          className="btn btn-success btn-lg"
-          onClick={() => props.onPlayerChoose("paper")}
-        >
-          Paper
-        </button>
-        <button
-          className="btn btn-success btn-lg"
-          onClick={() => props.onPlayerChoose("scissor")}
-        >
-          Scissor
-        </button>
+class ChoiceButtons extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="container d-flex justify-content-center">
+        <div className="">
+          <button
+            className="btn btn-success btn-lg"
+            onClick={() => this.props.onPlayerChoose("rock")}
+          >
+            Rock
+          </button>
+          <button
+            className="btn btn-success btn-lg"
+            onClick={() => this.props.onPlayerChoose("paper")}
+          >
+            Paper
+          </button>
+          <button
+            className="btn btn-success btn-lg"
+            onClick={() => this.props.onPlayerChoose("scissor")}
+          >
+            Scissor
+          </button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
+export default ChoiceButtons;
